cb0503aa9a8183fdb588d807595acce1a7801f55
048783 <048783@UNI-LAB2-35>
2025-02-24 20:14:38 -0300

    Atividade02

--- Atividade02/Program.cs
+++ Atividade02/Program.cs
@@ -1,24 +1,40 @@
-﻿using System.Reflection.PortableExecutable;
-
+﻿using System.ComponentModel.Design;
+using System.Reflection.PortableExecutable;
 Console.Write("Digite o seu sexo: ");
-string sexo  = Console.ReadLine().ToUpper();
+string sexo = Console.ReadLine().ToUpper();
 
-
-Console.Write("Digite a sua altura");
-double altura = double.Parse(Console.ReadLine());
-
-if (sexo == "M")
-{
-    double pesoIdeal = ((72.7 * altura) - 58);
-    Console.WriteLine($"Seu peso ideal seria:{pesoIdeal}");
-    if (sexo == "F")
-    {
-        pesoIdeal = ((62.1 * altura) - 44.7);
-        Console.WriteLine($"Seu peso ideal seria:{pesoIdeal}");
-    }
+do {
     
-}
-else
-{
-    Console.WriteLine("Sigla invalida, Digite M para masculino ou F para Feminino");
-}
+
+
+
+    if (sexo == "M" || sexo == "F") {
+
+        
+
+        if (sexo == "M")
+        {
+            Console.Write("Digite a sua altura: ");
+            double altura = double.Parse(Console.ReadLine());
+            double pesoIdeal = ((72.7 * altura) - 58);
+            Console.WriteLine($"Seu peso ideal seria: {pesoIdeal.ToString("F2")}");
+            return;
+        }
+        else
+        {
+            Console.Write("Digite a sua altura: ");
+            double altura = double.Parse(Console.ReadLine());
+            double pesoIdeal = ((62.1 * altura) - 44.7);
+            Console.WriteLine($"Seu peso ideal seria: {pesoIdeal.ToString("F2")}");
+            return;
+        }
+
+    }
+
+    else
+    {
+        Console.WriteLine("Sigla invalida, Digite M para masculino ou F para Feminino ou S para Sair");
+        Console.Write("Digite o seu sexo: ");
+         sexo = Console.ReadLine().ToUpper();
+    }
+} while (sexo != "S");
