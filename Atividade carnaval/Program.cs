d64e7bc63b0100f67bbadabf8a3b6988e817e569
048783 <048783@UNI-LAB2-35>
2025-02-24 19:47:20 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade carnaval/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Digite a venda média mensal: ");
+double mediaMensal = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o preço atual: ");
+double precoAtual = double.Parse(Console.ReadLine());
+
+
+
+if ((mediaMensal < 500) && (precoAtual < 30)) {
+    double acrescimo = 0.10;
+    precoAtual = ((precoAtual * acrescimo) + precoAtual);
+    Console.WriteLine($"O preço atual é: {precoAtual}");
+}
+else
+{
+    double reducao = 0.20;
+    precoAtual = (precoAtual - (precoAtual * reducao));
+    Console.WriteLine($"O preço atual é: {precoAtual}");
+}
---
+++ Atividade02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.PortableExecutable;
+
+Console.Write("Digite o seu sexo: ");
+string sexo  = Console.ReadLine().ToUpper();
+
+
+Console.Write("Digite a sua altura");
+double altura = double.Parse(Console.ReadLine());
+
+if (sexo == "M")
+{
+    double pesoIdeal = ((72.7 * altura) - 58);
+    Console.WriteLine($"Seu peso ideal seria:{pesoIdeal}");
+    if (sexo == "F")
+    {
+        pesoIdeal = ((62.1 * altura) - 44.7);
+        Console.WriteLine($"Seu peso ideal seria:{pesoIdeal}");
+    }
+    
+}
+else
+{
+    Console.WriteLine("Sigla invalida, Digite M para masculino ou F para Feminino");
+}
