085be3c91e81832791cda9eabd07e368ea73279e
048783 <048783@UNI-LAB2-35>
2025-02-24 21:04:49 -0300

    atividade04

---
+++ Atividade04/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Digite o número de horas trabalhadas no mês: ");
+double horasTrabalhadasMes = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o seu salário por hora: ");
+double salarioHora = double.Parse(Console.ReadLine());
+
+
+
+if (horasTrabalhadasMes > 160) {
+
+    double salarioMes = (salarioHora * 160);
+    double horasExtra = (horasTrabalhadasMes - 160);
+    double valorHorasExtra = (horasExtra * 0.5);
+    double salarioTotal = (salarioMes + valorHorasExtra);
+    Console.WriteLine($"O salario total é: {salarioTotal}");
+
+
+}
+
+
+
