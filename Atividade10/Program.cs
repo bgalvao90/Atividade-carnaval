a08b9e19b6ee196351cd4244268c472e4abf9911
usuario <usuario@DESKTOP-RSA23LP>
2025-03-09 00:23:23 -0300

    Robinho  Paga o pastel

---
+++ Atividade10/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] A = new int[4, 4];
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.WriteLine($"Digite {i} , {j} :");
+        A[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+int somaA = 0, somaB = 0, somaC = 0, somaD = 0;
+for (int i = 0;i < 4;i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        if (i < 2 && j < 2) somaA += A[i, j];
+        if (i >= 2 && j >= 2) somaB += A[i, j];
+        if (i > j) somaC += A[i, j];
+        if (i < j) somaD += A[i, j];
+    }
+}
+Console.WriteLine($"Soma da região (a): {somaA}");
+Console.WriteLine($"Soma da região (b): {somaB}");
+Console.WriteLine($"Soma da região (c): {somaC}");
+Console.WriteLine($"Soma da região (d): {somaD}");
